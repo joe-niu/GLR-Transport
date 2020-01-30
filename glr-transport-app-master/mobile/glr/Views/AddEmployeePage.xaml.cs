1bf32a0a86f46c00cabee339b8d8d633380d4869
joe-niu <joeniu@my.unt.edu>
2020-01-30 14:22:24 -0600

    Adding current glr transport project

---
+++ glr-transport-app-master/mobile/glr/Views/AddEmployeePage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using glr.Models;
+using Xamarin.Forms;
+
+namespace glr.Views
+{
+    public partial class AddEmployeePage : ContentPage
+    {
+        public AddEmployeePage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveEmployeeButtonClicked(object sender, EventArgs e)
+        {
+            var user = (User)BindingContext;
+
+            if (!user.IsaFieldEmpty())
+            {
+                if(user.TypeOfEmployee == -1) 
+                    await DisplayAlert("Employee type missing", "Must enter type of employee", "Ok");
+                else
+                {
+                    await App.Database.SaveUserAsync(user);
+                    await Navigation.PopAsync();
+                }
+            }
+            else
+            {
+                await DisplayAlert("Empty fields", "All fields must be filled out", "OK");
+            }
+        }
+
+        async void OnTypeButtonClicked(object sender, EventArgs e)
+        {
+            var user = (User)BindingContext;
+
+            if (user.IsaFieldEmpty()) await DisplayAlert("Empty fields", "All fields must be filled out before selecting a type", "OK");
+            else
+            {
+                var action = await DisplayActionSheet("Position", "Cancel",
+                null, "Manager", "Driver", "Employee");
+
+                if (action.Equals("Manager")) user.TypeOfEmployee = 0;
+                else if (action.Equals("Driver")) user.TypeOfEmployee = 1;
+                else if (action.Equals("Employee")) user.TypeOfEmployee = 2;
+            }
+        }
+    }
+}
---
+++ glr-transport-app-master/mobile/glr/Views/ViewEmployeeInfoPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using glr.Models;
+using SQLite;
+using Xamarin.Forms;
+using System.Data;
+
+namespace glr.Views
+{
+    public partial class ViewEmployeeInfoPage : ContentPage
+    {
+
+        public ViewEmployeeInfoPage(User employee)
+        {
+            InitializeComponent();
+
+            if (employee.TypeOfEmployee == 0) Position.Text = "Manager";
+            else if (employee.TypeOfEmployee == 1) Position.Text = "Driver";
+            else if (employee.TypeOfEmployee == 2) Position.Text = "Employee";
+
+            FirstNameLabel.Text = employee.FirstName;
+            LastNameLabel.Text = employee.LastName;
+            EmailAddressLabel.Text = employee.EmailAddress;
+        }
+
+        bool IsUser(User employee)
+        {
+            if(employee.FirstName.Equals(FirstNameLabel.Text)
+                && employee.LastName.Equals(LastNameLabel.Text)
+                && employee.EmailAddress.Equals(EmailAddressLabel.Text))
+            {
+                return true;
+            }return false;
+        }
+
+        async void OnDeleteEmployeeButtonClicked(object sender, EventArgs e)
+        {
+            var usersList = await App.Database.GetUsersAsync();
+            foreach(var emp in usersList)
+            {
+                if (IsUser(emp))
+                {
+                    await App.Database.DeleteUserAsync(emp);
+                    await Navigation.PopToRootAsync();
+                    break;
+                }
+            }
+        }
+    }
+}

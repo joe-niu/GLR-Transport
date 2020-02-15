582af0ae973e8dbde2f763c196b64bb0155f93cd
PeterMenchu <47905417+PeterMenchu@users.noreply.github.com>
2020-02-15 04:45:04 -0600

    Add files via upload

---
+++ Web/GlrTransport_Web_Interface/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GlrTransport_Web_Interface.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
